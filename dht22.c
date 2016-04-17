bf419476abf34d67cc92df19f72102377d826a95
mame-erl <mame-erl@users.noreply.github.com>
2016-04-17 15:09:50 +0200

    Update dht22.c

--- dht22.c
+++ dht22.c
@@ -1,8 +1,10 @@
 /*
- *      dht22.c:
+ *  dht22.c:
  *	Simple test program to test the wiringPi functions
  *	Based on the existing dht11.c
  *	Amended by technion@lolware.net
+ *
+ *  2016-02-17 Changed by Markus Meier for usage in thesis
  */
 
 #include <wiringPi.h>
@@ -12,12 +14,11 @@
 #include <stdint.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-#include "locking.h"
-
+#include <stdbool.h>
 #define MAXTIMINGS 85
 static int DHTPIN = 7;
 static int dht22_dat[5] = {0,0,0,0,0};
+
 
 static uint8_t sizecvt(const int read)
 {
@@ -34,7 +35,8 @@ static uint8_t sizecvt(const int read)
 
 static int read_dht22_dat()
 {
-  uint8_t laststate = HIGH;
+
+uint8_t laststate = HIGH;
   uint8_t counter = 0;
   uint8_t j = 0, i;
 
@@ -79,48 +81,28 @@ static int read_dht22_dat()
   // check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
   // print it out if data is good
   if ((j >= 40) && 
-      (dht22_dat[4] == ((dht22_dat[0] + dht22_dat[1] + dht22_dat[2] + dht22_dat[3]) & 0xFF)) ) {
-        float t, h;
-        h = (float)dht22_dat[0] * 256 + (float)dht22_dat[1];
-        h /= 10;
-        t = (float)(dht22_dat[2] & 0x7F)* 256 + (float)dht22_dat[3];
-        t /= 10.0;
-        if ((dht22_dat[2] & 0x80) != 0)  t *= -1;
-
-
-    printf("Humidity = %.2f %% Temperature = %.2f *C \n", h, t );
+      (dht22_dat[4] == ((dht22_dat[0] + dht22_dat[1] + dht22_dat[2] + dht22_dat[3]) & 0xFF)) )
+  {
+    float t, h;
+    h = (float)dht22_dat[0] * 256 + (float)dht22_dat[1];
+    h /= 10;
+    t = (float)(dht22_dat[2] & 0x7F)* 256 + (float)dht22_dat[3];
+    t /= 10.0;
+    if ((dht22_dat[2] & 0x80) != 0)  t *= -1;
+    printf("%.2f/%.2f\n", t, h );
     return 1;
   }
   else
   {
-    printf("Data not good, skip\n");
     return 0;
   }
 }
 
+
+
 int main (int argc, char *argv[])
 {
-  int lockfd;
-  int tries = 100;
-
-  if (argc < 2)
-    printf ("usage: %s <pin> (<tries>)\ndescription: pin is the wiringPi pin number\nusing 7 (GPIO 4)\nOptional: tries is the number of times to try to obtain a read (default 100)",argv[0]);
-  else
-    DHTPIN = atoi(argv[1]);
-   
-
-  if (argc == 3)
-    tries = atoi(argv[2]);
-
-  if (tries < 1) {
-    printf("Invalid tries supplied\n");
-    exit(EXIT_FAILURE);
-  }
-
-  printf ("Raspberry Pi wiringPi DHT22 reader\nwww.lolware.net\n") ;
-
-  lockfd = open_lockfile(LOCKFILE);
-
+ 	
   if (wiringPiSetup () == -1)
     exit(EXIT_FAILURE) ;
 	
@@ -130,13 +112,10 @@ int main (int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
-  while (read_dht22_dat() == 0 && tries--) 
-  {
-     delay(1000); // wait 1sec to refresh
-  }
-
-  delay(1500);
-  close_lockfile(lockfd);
-
-  return 0 ;
+	   delay(2000);
+	   while (read_dht22_dat() == 0) 
+	   {
+	      delay(1000); // wait 1sec to refresh
+       }
+  return 0;
 }
